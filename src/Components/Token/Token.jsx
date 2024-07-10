1db7614f813f189e0d73f3b2a9a72bafbaf99b83
unknown <rutwik2514@gmail.com>
2024-07-10 23:02:48 +0530

    redirects modified

--- src/Components/Token/Token.jsx
+++ src/Components/Token/Token.jsx
@@ -8,7 +8,7 @@ function Token() {
         let token =  Cookies.get("jwtToken")
         console.log("jwt token is", Cookies.get("jwtToken"));
         localStorage.setItem("user", token);
-        navigate("/home")
+        // navigate("/home")
     }, [token, navigate]);
   return (
     <p>{token}</p>
